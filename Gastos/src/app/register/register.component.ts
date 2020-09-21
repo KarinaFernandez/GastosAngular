e52fc92aa79f4c14d7cb93c9568a1409d034d648
Karina Fernandez <karina.fernandez@pedidosya.com>
2020-09-20 21:23:50 -0300

    Register form changed

--- Gastos/src/app/register/register.component.ts
+++ Gastos/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm;
+  errMsg;
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+  ) 
+  {
+    this.registerForm = this.formBuilder.group({
+      email: '',
+      password: ''
+    });
+  }
+
+  ngOnInit() {}
+
+  onSubmit(loginData) {
+    this.errMsg = undefined;
+  
+    this.registerForm.reset();
+
+    console.warn("Register information", loginData);
+
+    const { email, password } = loginData;
+
+    if (email == "" || password == "") {
+      this.errMsg = "Por favor valide los datos ingresados"
+    }
   }
 
 }
